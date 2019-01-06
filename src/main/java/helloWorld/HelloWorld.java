04ef21706b3d90c9082613dfa8661eb5a03a1779
ranjit-gill <44748186+ranjit-gill@users.noreply.github.com>
2019-01-06 11:44:44 +0530

    ghj

--- src/main/java/helloWorld/HelloWorld.java
+++ src/main/java/helloWorld/HelloWorld.java
@@ -4,10 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World...This is Ranjit!!!!");
-		System.out.println("I am a DevOps Engineer");
-		System.out.println("tESTING 123");
-		System.out.println("I am a DevOps Engineer");
+
 	}
 
 }
